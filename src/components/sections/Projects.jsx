8b8bb62feed37a6aa71f66582b9a2a1467d086e6
Peter-Lee00 <peter08520415@gmail.com>
2026-03-12 21:29:57 +0800

    update E-commerce app website URL

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -111,7 +111,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://e-commerce-nine-tan-58.vercel.app/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
